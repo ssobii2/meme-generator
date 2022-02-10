dbed98c51058ec0729d7495ac3fed5c7be7a4189
THUNDER-PC\subha <rntvtf@pte.hu>
2022-02-10 14:59:13 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+
+import React from "react"
+import Logo from "../images/troll-face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src={require=(Logo)} 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
